1338cb8e34e3880248ac444ab58cec0bc45db3d4
ivanocutrignelli <ivano.cutrignelli@edu.unifi.it>
2025-02-26 14:46:34 +0100

    aggiunta main, Cmakelist installazione librerie grafiche SFML

--- main.cpp
+++ main.cpp
@@ -1,7 +1,22 @@
 #include <iostream>
+#include <SFML/Graphics.hpp>
 
-int main()
-{
-    std::cout << "Hello, World!" << std::endl;
+int main() {
+    sf::RenderWindow window(sf::VideoMode(800, 600), "SFML su CLion");
+    sf::CircleShape shape(50);
+    shape.setFillColor(sf::Color::Blue);
+
+    while (window.isOpen()) {
+        sf::Event event;
+        while (window.pollEvent(event)) {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.clear();
+        window.draw(shape);
+        window.display();
+    }
+
     return 0;
 }
